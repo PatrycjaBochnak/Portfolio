9fbc3e8b61799fa486bc82fab0732d829970fa14
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2025-01-21 11:43:15 +0100

    Apperance mobile menu fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,16 +5,19 @@ import { BsFillPersonLinesFill } from "react-icons/bs";
 import { Link } from "react-scroll";
 
 const FILE_URL = "http://localhost:3000/files/PATRYCJABOCHNAK.pdf";
+
 const Navbar = () => {
   const [nav, setNav] = useState(false);
   const handleClick = () => setNav(!nav);
+
   const handleDownload = (url) => {
     fetch(url)
       .then((response) => {
         if (!response.ok) {
           throw new Error("Load failed");
         }
-        response.blob()})
+        return response.blob();
+      })
       .then((blob) => {
         const blobURL = window.URL.createObjectURL(new Blob([blob]));
         const fileName = url.split("/").pop();
@@ -28,10 +31,10 @@ const Navbar = () => {
   };
 
   return (
-    <div className="fixed w-full h-16 flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+    <div className="fixed w-full h-16 flex justify-between items-center px-4 bg-[#0a192f] text-gray-300 z-40">
       <div>Frontend dev portfolio</div>
 
-      {/* Menu xl */}
+      {/* Menu dla dużych ekranów */}
       <ul className="hidden md:flex space-x-4">
         {["home", "about", "skills", "projects", "contact"].map((section) => (
           <li key={section}>
@@ -42,31 +45,42 @@ const Navbar = () => {
         ))}
       </ul>
 
-      {/* Hamburger */}
+      {/* Menu mobilne */}
       <div className="md:hidden">
-        <div onClick={handleClick}>{!nav ? <FaBars /> : <FaTimes />}</div>
+        {/* Ikona Hamburgera */}
+        <div
+          onClick={handleClick}
+          className="absolute top-4 right-4 text-2xl text-gray-300 cursor-pointer z-50"
+        >
+          {!nav ? <FaBars /> : <FaTimes />}
+        </div>
+
+        {/* Menu */}
         {nav && (
-          <ul className="fixed bg-black z-50 opacity-100 top-16 left-0 w-full flex flex-col items-center">
-            {["home", "about", "skills", "projects", "contact"].map(
-              (section) => (
-                <li key={section} className="py-4">
-                  <Link
-                    onClick={handleClick}
-                    to={section}
-                    smooth={true}
-                    duration={500}
-                    offset={-50}
-                  >
-                    {section.charAt(0).toUpperCase() + section.slice(1)}
-                  </Link>
-                </li>
-              )
-            )}
-          </ul>
+          <div className="fixed inset-0 bg-[#0a192f] bg-opacity-100 z-40 flex flex-col items-center justify-center">
+            <ul className="space-y-8">
+              {["home", "about", "skills", "projects", "contact"].map(
+                (section) => (
+                  <li key={section} className="text-2xl text-white">
+                    <Link
+                      onClick={handleClick}
+                      to={section}
+                      smooth={true}
+                      duration={500}
+                      offset={-50}
+                      className="hover:text-gray-400"
+                    >
+                      {section.charAt(0).toUpperCase() + section.slice(1)}
+                    </Link>
+                  </li>
+                )
+              )}
+            </ul>
+          </div>
         )}
       </div>
 
-      {/* Social media icons */}
+      {/* Ikony społecznościowe */}
       <div className="hidden md:flex fixed top-[35%] left-0 flex-col">
         <ul>
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-gray-900">
